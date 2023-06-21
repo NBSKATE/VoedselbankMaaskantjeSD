bee9add042b9a6918d9f0b2c69a7187103cc282a
Milan Claase <328556@student.mboutrecht.nl>
2023-06-21 13:16:50 +0200

    eerste commit

--- app/config/config.php
+++ app/config/config.php
@@ -2,10 +2,9 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'VoedselbankMaaskantje');
+define('DB_NAME', 'examendag3');
 
 define('APPROOT', dirname(dirname(__FILE__)));
 
 define('URLROOT', 'http://voedselbankmaaskantje.com');
 define('SITENAME', 'Examen 2023');
-?>
